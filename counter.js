efa112969988c8ec207146ebff47e81e704c5b85
ygong10 <yichenggong567@gmail.com>
2016-03-22 22:59:43 -0500

    Need to fix timer

--- counter.js
+++ counter.js
@@ -1,12 +1,31 @@
 var counter = 0;
+var oldTime = new Date();
 
 function increment(){
 	counter++;
-	
 }
 
 function display(){
+	var currentTime = new Date();
+	var timeDifference = currentTime.getTime() - oldTime.getTime() 
+	document.getElementById('time').innerHTML = timeDifference/1000;
 	document.getElementById('cat_number').innerHTML =  counter;
 }
+
+
+function startTimer(){
+	var oldTime = new Date();
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
