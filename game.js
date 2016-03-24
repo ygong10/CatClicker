3b811811fb88bf0b95ec08bb2649b4dbe19924fb
Jung Woo (Edward) Kim <kim.edwardj@gmail.com>
2016-03-23 22:48:23 -0500

    Added food bowl class
    Food bowl class contains the text of the current amount of food and the
    image of the bowl. When clicked, the increment image appears above the
    bowl. Add Food button increases the increment.

---
+++ game.js
@@ -0,0 +1,57 @@
+var
+
+textrenderer,
+foodbowl
+
+
+;
+
+
+function initGame() {
+	var foodbowldim = constants.foodbowl;
+	foodbowl = new FoodBowl(foodbowldim.x, foodbowldim.y, foodbowldim.width, foodbowldim.height);
+	
+	textrenderer = new EffectsTextPool();
+	textrenderer.spawnText("+1", 200, 200, constants.font.displaydim.width, constants.font.displaydim.height);
+	textrenderer.spawnText("23", 250, 200, constants.font.displaydim.width, constants.font.displaydim.height);
+}
+
+document.onclick = function(e) {
+	var mousePos = getMousePos(e);
+	//textrenderer.spawnText("+1", mousePos.x, mousePos.y, constants.font.displaydim.width, constants.font.displaydim.height);
+	foodbowl.incrementFoodCount(counter);
+}
+
+function getMousePos(e) {
+	var rect = canvas.getBoundingClientRect();
+	return {
+	x: e.clientX - rect.left,
+	y: e.clientY - rect.top
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
